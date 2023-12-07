4f47961715fbad068b67ff3866e35b61388c3f93
HsiaoTingHong <hsiaoting1003@gmail.com>
2023-12-07 15:56:47 +0800

    vue practice2 dist second commit

---
+++ js/about.c4fee45a.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkproject02"]=self["webpackChunkproject02"]||[]).push([[443],{833:function(n,t,e){e.r(t),e.d(t,{default:function(){return l}});var r=e(252);const u={class:"about"},a=(0,r._)("h1",null,"This is an about page",-1),c=[a];function s(n,t){return(0,r.wg)(),(0,r.iD)("div",u,c)}var o=e(744);const i={},f=(0,o.Z)(i,[["render",s]]);var l=f}}]);
+//# sourceMappingURL=about.c4fee45a.js.map
